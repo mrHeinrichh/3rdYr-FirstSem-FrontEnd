f3c81f71136b62c83d20c09816a524ff76d51e6e
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2022-11-23 05:57:27 +0800

    Add operator model

---
+++ app/Models/operator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class operator extends Model
+{
+    use HasFactory;
+    public $table = "operator";
+    // public $timestamps = false;
+    public $primaryKey = "operator_id";
+    protected $fillable = [
+        "name",
+        "contact_number",
+        "age",
+        "address",
+        "image_path"
+    ];
+}
