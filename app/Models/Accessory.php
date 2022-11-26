0d24d0a9522aa48d6ba44f6f5185e228083a88c4
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-11-26 21:08:19 +0800

    Add Accessory model

---
+++ app/Models/Accessory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Accessory extends Model
+{
+    use HasFactory;
+
+
+    public $primarykey = 'id';
+
+    protected $fillable = [
+        "description",
+        "quantity",
+        "costs",
+    ];
+}
