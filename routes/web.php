33e64e934bb51d858e601171c0a4b8934e7a63e7
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-11-26 20:49:20 +0800

    Default web routes

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/login', function () {
-    return view('login');
-});
-
-Route::get('/signup', function () {
-    return view('signup');
-});
-
-Route::resource('operator', 'operatorController');
-
-route::view('/operator-index', 'operator.index');
-
-Route::resource('service', 'serviceController');
-
-route::view('/service-index', 'service.index');
